16473e3c07840b856cb54b147529891da3d2c01e
Omatsola Sobotie <tsola2002@yahoo.co.uk>
2016-05-19 06:35:30 +0100

    added gulp watcher & live reload to our workflow

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     browserify = require('gulp-browserify'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    connect = require('gulp-connect'),
+    less = require('gulp-less');
 
 //javascript files that need to be combined
 
@@ -22,6 +24,27 @@ var jsSources = [
     'components/js/transition.js'
 ];
 
+//less files that need to be processed
+
+var lessSources = ['components/less/bootstrap.less'];
+
+//html files that need to be processed
+
+var htmlSources = ['builds/development/*.html'];
+
+gulp.task('less', function(){
+    //specify where less files are located
+    gulp.src(lessSources)
+        .pipe(less())
+        //spit log message if there are any errors
+        .on('error', gutil.log)
+        //output final file to destination folder
+        .pipe(gulp.dest('builds/development/css'))
+        //do a reload on the server
+        .pipe(connect.reload())
+});
+
+
 gulp.task('log', function(){
     //output piece of text to the console
     gutil.log('Workflows are awesome');
@@ -37,4 +60,41 @@ gulp.task('combine-js', function() {
         .pipe(browserify())
         //output final file to destination folder
         .pipe(gulp.dest('builds/development/js'))
+        //do a reload on the server
+        .pipe(connect.reload())
 });
+
+gulp.task('watch', function() {
+    //when any lessSources file changes run less method
+    gulp.watch(lessSources, ['less']);
+    //when any jsSources file changes run combine-js method
+    gulp.watch(jsSources, ['combine-js']);
+    //when any file with a .less extension changes, we run the less task
+    gulp.watch('components/less/*.less', ['less']);
+    //when any html file changes do a livereload
+    gulp.watch(htmlSources, ['html']);
+});
+
+gulp.task('connect', function() {
+    //use connect variable's of the server method to create a server
+    connect.server({
+        //specify the root of your application
+        root: 'builds/development/',
+        //turn on livereload feature
+        livereload: true
+    });
+});
+
+gulp.task('html', function() {
+    //set input sources to html files
+    gulp.src(htmlSources)
+        //pipe the sources to livereload
+        .pipe(connect.reload())
+});
+
+
+
+
+
+//custom gulp task to run all tasks
+gulp.task('default', ['html', 'less', 'combine-js','log', 'connect', 'watch']);
